6083ae59d0b909bf68b9ac237242bafa1a0223ca
Juan David Salazar Arcila <juan.salazar@tecnofactor.com>
2022-07-28 19:24:42 -0500

    Estructura del proyecto

---
+++ src/main/java/com/example/serviciosrest/ServiciosrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviciosrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serviciosrest/ServiciosrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviciosrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
