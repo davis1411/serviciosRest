b70f59437fde21ef3e9d300f9c6efce7d1f78c34
Juan David Salazar Arcila <juan.salazar@tecnofactor.com>
2022-08-02 18:26:23 -0500

    Adiciona metodo post

---
+++ src/main/java/Model/Movie.java
@@ -0,0 +1,38 @@
+package Model;
+
+public class Movie {
+
+    private String id;
+    private String nombre;
+    private String genero;
+
+    public Movie(String id, String nombre, String genero){
+        this.id = id;
+        this.nombre = nombre;
+        this.genero = genero;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
--- src/main/java/controller/MovieController.java
+++ src/main/java/controller/MovieController.java
@@ -1,14 +1,13 @@
 package controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import Model.Movie;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/api")
 public class MovieController {
 
     @GetMapping(value="/listMovies")
@@ -20,5 +19,27 @@ public class MovieController {
         movies.add("Thor");
         movies.add("Guardianes de la galaxia");
         return movies;
+    }
+
+    @GetMapping("getMovieById/{id}")
+    public Movie getMovieById(@PathVariable String id){
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("1","Capitán America", "Accion"));
+        movies.add(new Movie("2","Iron man", "Accion"));
+        movies.add(new Movie("3","Los vengadores", "Accion"));
+        movies.add(new Movie("4","Thor", "Accion"));
+        movies.add(new Movie("5","Guardianes de la galaxia", "Accion"));
+
+        for(Movie movie : movies){
+            if(id.equals(movie.getId())){
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/postWelcomeMovies")
+    public String postMovies(@RequestBody String name){
+        return "Bienvenido " + name + " a la cartelera de peliculas" ;
     }
 }
